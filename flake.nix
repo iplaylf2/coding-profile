4d49051811aaa5eea96706a4fe8e73b84d4b014a
i-play-lf2 <914637118@qq.com>
2026-02-01 03:13:47 +0000

    refactor: modernize nix configuration with flake setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python314
+            uv
+          ];
+        }
+      );
+    };
+}
